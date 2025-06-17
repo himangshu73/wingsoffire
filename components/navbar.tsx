1ccdee881305a3f4855a2474d3b09150bff9ca33
himangshu <himangshu73@gmail.com>
2025-06-17 07:19:59 +0600

    layout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,31 +5,33 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
   return (
-    <header className="flex items-center justify-between gap-2 px-3 py-2 bg-white shadow-md">
-      <Link
-        href="/"
-        title="Go to homepage"
-        className="flex items-center min-w-[50px]"
-      >
-        <Image
-          src="/logo-transparent.png"
-          width={160}
-          height={40}
-          alt="logo"
-          className="hidden md:block"
-        />
-        <h1 className="text-xl md:hidden font-extrabold">
-          <span className="text-blue-700">F</span>
-          <span className="text-orange-700">S</span>
-        </h1>
-      </Link>
-      <div className="flex-1 max-w-xs sm:max-w-sm">
-        <SearchBox />
-      </div>
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto px-4 flex items-center justify-between gap-2 py-2">
+        <Link
+          href="/"
+          title="Go to homepage"
+          className="flex items-center flex-shrink-0"
+        >
+          <Image
+            src="/logo-transparent.png"
+            width={160}
+            height={40}
+            alt="logo"
+            className="hidden md:block"
+          />
+          <h1 className="text-xl md:hidden font-extrabold">
+            <span className="text-blue-700">Fire</span>
+            <span className="text-orange-700">Shop</span>
+          </h1>
+        </Link>
+        <div className="flex-1 max-w-xs sm:max-w-sm px-4">
+          <SearchBox />
+        </div>
 
-      <button className="text-2xl md:hidden ml-2" title="Menu">
-        <GiHamburgerMenu />
-      </button>
+        <button className="text-2xl md:hidden ml-2" title="Menu">
+          <GiHamburgerMenu />
+        </button>
+      </div>
     </header>
   );
 }
